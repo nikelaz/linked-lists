21dc6c233c64a42bfd534a09a23e8f589051fec2
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2022-06-26 10:39:02 +0300

    restructure files

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-#include "Node.h"
-#include "LinkedList.h"
+
+namespace Singly {
+  #include "./singly-linked-list/Node.h"
+  #include "./singly-linked-list/LinkedList.h"
+}
 
 int main() {
-  LinkedList<int>* list1 = new LinkedList<int>();
+  /* Singly Linked List Demo
+   ************************************** */
+  Singly::LinkedList<int>* list1 = new Singly::LinkedList<int>();
 
+  list1->push(new Singly::Node<int>(1));
+  list1->push(new Singly::Node<int>(2));
+  list1->push(new Singly::Node<int>(3));
 
-  list1->push(new Node<int>(1));
-  list1->push(new Node<int>(2));
-  list1->push(new Node<int>(3));
+  Singly::Node<int>* i = list1->getHead();
 
-  // Print all items from the list
-  Node<int>* i = list1->getHead();
+  std::cout << "Singly Linked List Demo" << std::endl;
 
   while (i != nullptr) {
     std::cout << i->getData() << std::endl;
--- src/singly-linked-list/LinkedList.h
+++ src/singly-linked-list/LinkedList.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Node.h"
 
 template <class T>
