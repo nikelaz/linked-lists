baa9ea3611a9b10076c37fe2fa30401b4463a672
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2022-07-09 10:29:07 +0300

    add premake5 config

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "LinkedLists"
+   configurations { "Debug", "Release" }
+
+project "LinkedLists"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
